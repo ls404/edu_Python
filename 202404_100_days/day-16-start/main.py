246948f90168e769aa73624dba66742bb3862ca6
Lubo S <16446408+ls404@users.noreply.github.com>
2024-05-04 22:57:55 +0300

    :rocket:

---
+++ 202404_100_days/day-16-start/main.py
@@ -0,0 +1,17 @@
+import another_module
+
+print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+
+tummy = Turtle()
+
+my_screen = Screen()
+tummy.shape("turtle")
+tummy.color("coral")
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+my_screen.exitonclick()
+
+
+
