7deec9d1f52bd8b50d7602b0c4f413340a962d6c
Lubo S <16446408+ls404@users.noreply.github.com>
2024-06-23 18:19:05 +0300

    :rocket:
    Took 8 minutes

--- 202404_100_days/day-16-start/coffee/main.py
+++ 202404_100_days/day-16-start/coffee/main.py
@@ -22,4 +22,4 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+               coffee_maker.make_coffee(drink)
